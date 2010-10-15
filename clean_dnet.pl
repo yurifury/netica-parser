9463359d5bf468ef849334214468238e839a327c
Yuri Feldman <yuri@tbqh.net>
2010-10-15 11:12:27 +1100

    added list parsing, ignoring certain fields

--- clean_dnet.pl
+++ clean_dnet.pl
@@ -5,11 +5,16 @@ use warnings;
 use Data::Dumper;
 use Dnet;
 
+my $filename = 'Exam.dnet';
+if (defined $ARGV[0]) {
+    $filename = $ARGV[0];
+}
+    
 my @contents;
 { 
     local $/ = "\n";
     local *FILE;
-    open FILE, "<Exam.dnet";
+    open FILE, "<", $filename;
     @contents = <FILE>;
     close FILE 
 }
@@ -27,34 +32,76 @@ map {
     s/\s*$//g;
     s/^\s*//g;
     } @contents;
-#print Dumper @contents;
 
-
+#print Dumper $dnet;
 my $dnet = Dnet->new();
 my $in_node = 0;
 
+foreach my $x (@contents) {
+    #print "$x\n\n";
+}
+
 for (@contents) {
-    if ($in_node) {
-        if (/(\w+) = (\w+)/) {
-            $dnet->{$in_node}->{$1} = $2;
-        }
-        if (/(\S+) = \(((?:\S?,?\s*)+)\)/) {
-            my @list = split /,/, $2;
-            s/\s//g for @list;
-            #print Dumper @list;
-            $dnet->{$in_node}->{$1} = [@list];
-        }
-    }
-    if (/bnet (\w+)/) {
-        $dnet->{_name} = $1;
-    }
+    # If we've found a node, make a new node in dnet
+    # Also get ready to pull this node's attributes
     if (/node (\w+)/) {
         my $node_name = $1;
         $in_node = $1;
         $dnet->{$1} = {};
     }
+    # We're in the attributes of a node
+    if ($in_node) {
+        # Fields to ignore
+        next if /whenchanged|center|height/;
+        # Parse a single key/value
+        if (/(\w+) = (\w+)/) {
+            $dnet->{$in_node}->{$1} = $2;
+        }
+        # Parse a list
+        if (/(\S+) = (\((?:\S?,?\s*)+\))/) {
+            #my @list = split /,/, $2;
+            my $list = parse_list($2);
+            #print Dumper @list;
+            $dnet->{$in_node}->{$1} = $list;
+        }
+    }
+    # Set the name of the BN when we've found it
+    if (/bnet (\w+)/) {
+        $dnet->{_name} = $1;
+    }
+}
+print Dumper $dnet;
+
+my %ordering = $dnet->create_ordered_hash();
+print "Node: Parents\n";
+print "-------------\n";
+foreach my $key (sort keys %ordering) {
+    print "$key:\n";
+    foreach my $x (@{$ordering{$key}}) {
+        print "  $x\n";
+        # body...
+    }
+    print "\n";
 }
 
-#print Dumper $dnet;
+print "Priors:\n";
+foreach my $prior ($dnet->list_priors()) {
+    print "  $prior\n";
+}
+#print Dumper $dnet->list_priors();
 
-print Dumper $dnet->list_priors();
+sub parse_list {
+    my $list = shift;
+
+    $list =~ s/^,//g;
+    $list =~ s/\s//g;
+
+    $list =~ s/(\w+)/"$1"/g;
+
+    $list =~ s/\(/[/g;
+    $list =~ s/\)/]/g;
+
+    my $parsed_list = eval $list;
+    #print Dumper $parsed_list;
+    return $parsed_list;
+}
