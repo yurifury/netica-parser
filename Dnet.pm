054f26f0635b05bb7ffb9764879c3078689b9441
Yuri Feldman <yuri@tbqh.net>
2010-10-19 16:19:26 +1100

    putting parsing into dnet objects

--- Dnet.pm
+++ Dnet.pm
@@ -1,40 +1,209 @@
+package Dnet;
+
 use warnings;
 use strict;
 use Data::Dumper;
 
-package Dnet;
 sub new {
     my $self = {};
     bless $self;
     return $self;
 }
 
-sub list_priors {
+#sub get_nodes {
+    #my $self = shift;
+
+#}
+
+#sub list_leaves {
+    #my $self = shift;
+
+    #my %leaves = %nodes;
+    #foreach my $node (sort keys %nodes) {
+        #foreach my $parent (@{$order_nodes{$node}}) {
+            #delete $leaves{$parent} if defined $leaves{$parent};
+        #}
+    #}
+    #return keys %leaves;
+#}
+
+#sub list_roots {
+    #my $self = shift;
+    #my @roots = ();
+    #foreach my $node (sort keys %{$self}) {
+        #next if $node =~ /^_/;
+        #if (defined $self->{$node}->{parents}
+                #and @{$self->{$node}->{parents}} == 0) {
+            #push @roots, $node;
+        #}
+    #}
+    #@root_nodes = @roots;
+    #return @roots;
+#}
+
+#sub create_ordered_hash {
+    #my $self = shift;
+
+    #my %ordering = ();
+    #foreach my $node (sort keys %{$self}) {
+        #next if $node =~ /^_/;
+        #if (defined $self->{$node}->{parents}) {
+            #foreach my $parent (@{$self->{$node}->{parents}}) {
+                #push @{$ordering{$node}}, $parent;
+                ##print "${node}'s parent is $parent\n";
+            #}
+        #}
+    #}
+    #%order_nodes = %ordering;
+    #return %ordering;
+#}
+
+#sub partial_ordering {
+    #my $self = shift;
+
+    #unless (@root_nodes and %order_nodes) {
+        #$self->create_ordered_hash();
+        #$self->list_roots();
+    #}
+
+    #my %partial = ();
+
+    #%partial = map {$_ => undef } keys %leaves;
+
+    #print "\nPartial ordering:\n";
+    #foreach my $key (sort keys %partial) {
+        #$partial{$key} = 'undef' unless defined $partial{$key};
+        #print "$key => $partial{$key}\n";
+    #}
+
+
+#}
+
+sub parse {
     my $self = shift;
-    my @priors = ();
-    foreach my $node (sort keys %{$self}) {
-        next if $node =~ /^_/;
-        if (defined $self->{$node}->{parents}
-                and @{$self->{$node}->{parents}} == 0) {
-            push @priors, $node;
+
+    my $in_node = 0;
+
+    foreach (@_) {
+        # If we've found a node, make a new node in dnet
+        # Also get ready to pull this node's attributes
+        if (/node (\S+)/) {
+            my $node_name = $1;
+            $in_node = $1;
+            $self->{$1} = {};
+        }
+        # We're in the attributes of a node
+        if ($in_node) {
+            # Fields to ignore
+            next if /comment|whenchanged|center|height/;
+            # Parse a single key/value
+            if (/(\S+) = (\S+)/) {
+                $self->{$in_node}->{$1} = $2;
+            }
+            # Parse a list
+            if (/(\S+) = (\((?:\S?,?\s*)+\))/) {
+                #my @list = split /,/, $2;
+                my $list = $self->parse_list($2);
+                #print Dumper @list;
+                $self->{$in_node}->{$1} = $list;
+            }
+        }
+        # Set the name of the BN when we've found it
+        if (/bnet (\S+)/) {
+            $self->{_name} = $1;
         }
     }
-    return @priors;
-}
-
-sub create_ordered_hash {
-    my $self = shift;
-
-    my %ordering = ();
+    
+    $self->{_nodes} = [];
     foreach my $node (sort keys %{$self}) {
         next if $node =~ /^_/;
+        push @{$self->{_nodes}}, $node;
+    }
+
+    $self->{_roots} = [];
+    foreach my $node (@{$self->{_nodes}}) {
+        if (defined $self->{$node}->{parents}
+                and @{$self->{$node}->{parents}} == 0) {
+            push @{$self->{_roots}}, $node;
+        }
+    }
+
+    $self->{_ordering} = {};
+    foreach my $node (@{$self->{_nodes}}) {
         if (defined $self->{$node}->{parents}) {
             foreach my $parent (@{$self->{$node}->{parents}}) {
-                push @{$ordering{$node}}, $parent;
-                #print "${node}'s parent is $parent\n";
+                push @{$self->{_ordering}->{$node}}, $parent;
             }
         }
     }
-    return %ordering;
+
+    #%partial = map {$_ => undef } keys %leaves;
+    my %leaves = map {$_ => 1} @{$self->{_nodes}};
+    foreach my $node (@{$self->{_nodes}}) {
+        foreach my $parent (@{$self->{_ordering}->{$node}}) {
+            delete $leaves{$parent} if defined $leaves{$parent};
+        }
+    }
+
+    @{$self->{_leaves}} = keys %leaves;
+    #foreach my $x (@{$self->{_roots}}) {
+        #print "$x\n";
+        ## body...
+    #}
+
+#my %nodes;
+#my @root_nodes;
+#my %order_nodes;
+#my %leaves;
+
 }
+
+sub parse_list {
+    my $self = shift;
+    my $list = shift;
+
+    $list =~ s/^,//g;
+    $list =~ s/\s//g;
+
+    $list =~ s/([\w.]+)/"$1"/g;
+
+    $list =~ s/\(/[/g;
+    $list =~ s/\)/]/g;
+
+    my $parsed_list = eval $list;
+    #print Dumper $parsed_list;
+    return $parsed_list;
+}
+
+sub create_from_file {
+    my $self = shift;
+    my $filename = shift;
+
+    my @contents;
+    { 
+        local $/ = "\n";
+        local *FILE;
+        open FILE, "<", $filename;
+        @contents = <FILE>;
+        close FILE 
+    }
+
+    map { 
+    s/\/\/.*$//g;
+    } @contents;
+
+    my $file = join "", @contents;
+    @contents = split /[;{}]/, $file;
+    @contents = grep { !/^\s*$/ } @contents;
+    map { 
+    s/\/\/.*$//g;
+    s/\s+/ /g;
+    s/\s*$//g;
+    s/^\s*//g;
+    } @contents;
+
+
+    $self->parse(@contents);
+}
+
 1;
--- clean_dnet.pl
+++ clean_dnet.pl
@@ -9,99 +9,35 @@ my $filename = 'Exam.dnet';
 if (defined $ARGV[0]) {
     $filename = $ARGV[0];
 }
-    
-my @contents;
-{ 
-    local $/ = "\n";
-    local *FILE;
-    open FILE, "<", $filename;
-    @contents = <FILE>;
-    close FILE 
-}
-
-map { 
-    s/\/\/.*$//g;
-    } @contents;
-
-my $file = join "", @contents;
-@contents = split /[;{}]/, $file;
-@contents = grep { !/^\s*$/ } @contents;
-map { 
-    s/\/\/.*$//g;
-    s/\s+/ /g;
-    s/\s*$//g;
-    s/^\s*//g;
-    } @contents;
-
-#print Dumper $dnet;
 my $dnet = Dnet->new();
-my $in_node = 0;
-
-foreach my $x (@contents) {
-    #print "$x\n\n";
-}
-
-for (@contents) {
-    # If we've found a node, make a new node in dnet
-    # Also get ready to pull this node's attributes
-    if (/node (\S+)/) {
-        my $node_name = $1;
-        $in_node = $1;
-        $dnet->{$1} = {};
-    }
-    # We're in the attributes of a node
-    if ($in_node) {
-        # Fields to ignore
-        next if /comment|whenchanged|center|height/;
-        # Parse a single key/value
-        if (/(\S+) = (\S+)/) {
-            $dnet->{$in_node}->{$1} = $2;
-        }
-        # Parse a list
-        if (/(\S+) = (\((?:\S?,?\s*)+\))/) {
-            #my @list = split /,/, $2;
-            my $list = parse_list($2);
-            #print Dumper @list;
-            $dnet->{$in_node}->{$1} = $list;
-        }
-    }
-    # Set the name of the BN when we've found it
-    if (/bnet (\S+)/) {
-        $dnet->{_name} = $1;
-    }
-}
+$dnet->create_from_file($filename);
 print Dumper $dnet;
 
-my %ordering = $dnet->create_ordered_hash();
-print "Node: Parents\n";
-print "-------------\n";
-foreach my $key (sort keys %ordering) {
-    print "$key:\n";
-    foreach my $x (@{$ordering{$key}}) {
-        print "  $x\n";
-        # body...
-    }
-    print "\n";
-}
+#my %ordering = $dnet->create_ordered_hash();
+#print "Node: Parents\n";
+#print "-------------\n";
+#foreach my $key (sort keys %ordering) {
+    #print "$key:\n";
+    #foreach my $x (@{$ordering{$key}}) {
+        #print "  $x\n";
+    #}
+    #print "\n";
+#}
 
-print "Priors:\n";
-foreach my $prior ($dnet->list_priors()) {
-    print "  $prior\n";
-}
-#print Dumper $dnet->list_priors();
+#print "Nodes:\n";
+#foreach my $root ($dnet->get_nodes()) {
+    #print "  $root\n";
+#}
 
-sub parse_list {
-    my $list = shift;
+#print "Roots:\n";
+#foreach my $root ($dnet->list_roots()) {
+    #print "  $root\n";
+#}
 
-    $list =~ s/^,//g;
-    $list =~ s/\s//g;
+#print "Leaves:\n";
+#foreach my $root ($dnet->list_leaves()) {
+    #print "  $root\n";
+#}
 
-    $list =~ s/([\w.]+)/"$1"/g;
+#$dnet->partial_ordering();
 
-    $list =~ s/\(/[/g;
-    $list =~ s/\)/]/g;
-
-    my $parsed_list = eval $list;
-    #print Dumper $parsed_list;
-    return $parsed_list;
-}
